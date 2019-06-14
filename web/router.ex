fdb945cb49f65009ad7bc87911823a57c8ce928f
Greg Donald <gdonald@gmail.com>
2019-06-13 20:12:59 -0500

    User,Topic models, Github OAuth

---
+++ web/router.ex
@@ -0,0 +1,29 @@
+defmodule Discuss.Router do
+  use Discuss.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/auth", Discuss do
+    pipe_through :browser
+
+    get "/signout", AuthController, :signout
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
+  end
+
+  scope "/", Discuss do
+    pipe_through :browser
+    resources "/", TopicController
+  end
+end
